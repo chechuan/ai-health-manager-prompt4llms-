5b41b15d6f0497182aa2ded32d55a06177dfebab
songhaoyanga <songhaoyanga@enn.cn>
2023-11-01 10:35:55 +0800

    update func chat_qwen description

--- src/prompt/model_init.py
+++ src/prompt/model_init.py
@@ -20,16 +20,45 @@ openai.api_base = api_config['llm'] + "/v1"
 openai.api_key = "EMPTY"
 model="Qwen-14B-Chat"
 
-def chat_qwen(query: str = "", history: List[Dict] = []):
+def chat_qwen(query: str = "", history: List[Dict] = [], **kwargs):
+    """chat with qwen api which is serve at http://10.228.67.99:26921
+    
+    List options
+
+    Args:
+        query (string or null, Required):
+            Can be None or emtpy string.
+            If query, history will append {{"role":"user", "content": query}}
+        history (`array[Dict]`, [], Required):
+            A list of messages comprising the conversation so far.
+        top_k (`float`):
+            Adding some randomness helps make output text more natural. 
+            In top-k decoding, we first shortlist three tokens then sample one of them considering their likelihood scores.
+        top_p (`number` or `null` Optional Defaults to 0.5):
+            An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. 
+            So 0.1 means only the tokens comprising the top 10% probability mass are considered.
+            We generally recommend altering this or temperature but not both.
+        repetition_penalty (`float`):
+            The parameter for repetition penalty. 1.0 means no penalty.
+        temperature (number or null Optional Defaults to 0.7): 
+            What sampling temperature to use, between 0 and 2. 
+            Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic.
+    """
+    top_k = kwargs.get("top_k", 0)
+    top_p = kwargs.get("top_p", 0.5)
+    repetition_penalty = kwargs.get("repetition_penalty", 1.1)
+    temperature = kwargs.get("temperature", 0.5)
+
     if isinstance(query, str) and query:
         history += [{"role": "user", "content": query}]
+    
     completion = openai.ChatCompletion.create(
         model=model,
         messages=history,
-        top_k=0, 
-        top_p=0.5, 
-        repetition_penalty=1.1,
-        temperature=0.5
+        top_k=top_k, 
+        top_p=top_p, 
+        repetition_penalty=repetition_penalty,
+        temperature=temperature
     )
     ret = completion['choices'][0]['message']['content'].strip()
     return ret
