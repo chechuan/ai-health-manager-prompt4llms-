d75a93f97a9102d950fbbb11c3a59ba0afa909b3
yuanhuachao <yuanhuachao@enn.cn>
2023-11-22 18:05:38 +0800

    .

--- server.py
+++ server.py
@@ -109,8 +109,8 @@ def create_app():
         global chat
         try:
             param = accept_param()
-            item = chat.intent_query(param.get('history',[]), param.get('task',
-                ''), param.get('prompt', ''))
+            item = chat.intent_query(param.get('history',[]), task=param.get('task',
+                ''), prompt=param.get('prompt', ''))
             result = make_result(items=item)
         except AssertionError as err:
             logger.exception(err)
