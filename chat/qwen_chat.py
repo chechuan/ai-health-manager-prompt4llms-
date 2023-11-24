a0e0e5360a7f9ae53700c81a5ec5a3a24fe03565
yuanhuachao <yuanhuachao@enn.cn>
2023-11-24 15:52:46 +0800

    .

--- chat/qwen_chat.py
+++ chat/qwen_chat.py
@@ -267,9 +267,10 @@ class Chat:
         """获取用户信息
         """
         logger.debug('信息提取prompt为：' + prompt)
-        content = chat_qwen(prompt, verbose=False, temperature=0.7, top_p=0.8,
+        model_output = chat_qwen(prompt, verbose=False, temperature=0.7, top_p=0.8,
                 max_tokens=200, do_sample=False)
         logger.debug('信息提取模型输出：' + content)
+        content = model_output.split('\n')[0].split('。')[0]
         self.update_mid_vars(mid_vars, key="获取用户信息 01", input_text=prompt, output_text=content, model="Qwen-14B-Chat")
         if sum([i in content for i in ["询问","提问","转移","未知","结束", "停止"]]) != 0:
             logger.debug('信息提取流程结束...')
