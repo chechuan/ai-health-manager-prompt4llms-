9b0e2d68e0fc16f8797a80c637131183071dc68f
yuanhuachao <yuanhuachao@enn.cn>
2023-12-17 13:31:55 +0800

    .

---
+++ chat/util.py
@@ -0,0 +1,13 @@
+def get_role_from_intent(intentCode):
+    if inentCode in []:
+        return 'feldersher'
+
+def get_roleDesc_from_role(roel):
+    if role == 'feldersher':
+        return ''
+
+def get_need_from_role(role):
+    pass
+
+def get_constaint_from_role(role):
+    pass
