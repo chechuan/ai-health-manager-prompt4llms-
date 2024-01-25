ed93f4a4f4edc120d8f33d58e9197a93db70a8fa
songhaoyanga <songhaoyanga@enn.cn>
2024-01-25 15:47:42 +0800

    update streamlit logger

--- src/test/exp/Chatbot.py
+++ src/test/exp/Chatbot.py
@@ -7,11 +7,22 @@
 '''
 
 import os
-from platform import system
+from pathlib import Path
 
 import streamlit as st
 from loguru import logger
 from openai import OpenAI
+
+logger.add(
+    Path("logs", "chatbot.log"), 
+    rotation="10 MB", 
+    encoding="utf-8",
+    rotation="00:00",
+    retention="10 days",
+    compression="gz",
+    backtrace=True,
+    diagnose=True
+)
 
 client = OpenAI()
 
@@ -20,39 +31,53 @@ default_system_prompt = """你是一个经验丰富的医生，同时又是一�
 2. 问题关键点可以包括：持续时间、发生时机、诱因或症状发生部位等, 注意同类问题可以总结在一起问
 3. 最后请你结合获取到的信息给出我的诊断结果，可以是某种疾病，或者符合描述的中医症状，并解释给出这个诊断结果的原因，以及对应的处理方案
 """
+
+
 class Args:
     ...
 
+
 args = Args()
+
 
 def prepare_parameters():
     """Initialize the parameters for the llm"""
     global args
-    args.max_tokens = st.sidebar.slider("Max tokens", min_value=1, max_value=32000, value=4096, step=1)
-    args.temperature = st.sidebar.slider("Temperature", min_value=0.0, max_value=2.0, value=0.7, step=0.1)
-    args.top_p = st.sidebar.slider("Top p", min_value=0.0, max_value=1.0, value=0.8, step=0.1)
+    args.max_tokens = st.sidebar.slider(
+        "Max tokens", min_value=1, max_value=32000, value=4096, step=1)
+    args.temperature = st.sidebar.slider(
+        "Temperature", min_value=0.0, max_value=2.0, value=0.7, step=0.1)
+    args.top_p = st.sidebar.slider(
+        "Top p", min_value=0.0, max_value=1.0, value=0.8, step=0.1)
     # args.top_k = st.sidebar.slider("Top k", min_value=-1, max_value=100, value=-1, step=1)
     args.n = st.sidebar.slider("N", min_value=1, max_value=50, value=1, step=1)
-    args.presence_penalty = st.sidebar.slider("Presence penalty", min_value=0.0, max_value=2.0, value=0.0, step=0.1)
-    args.frequency_penalty = st.sidebar.slider("Frequency penalty", min_value=0.0, max_value=2.0, value=0.0, step=0.1)
+    args.presence_penalty = st.sidebar.slider(
+        "Presence penalty", min_value=0.0, max_value=2.0, value=0.0, step=0.1)
+    args.frequency_penalty = st.sidebar.slider(
+        "Frequency penalty", min_value=0.0, max_value=2.0, value=0.0, step=0.1)
     args.stop = st.sidebar.text_input("Stop words(split with `,`)", value="")
+
 
 def initlize_system_prompt():
     """Initialize the system prompt"""
     st.session_state.messages = []
-    st.session_state.messages.append({"role": "system", "content": st.session_state.system_prompt})
+    st.session_state.messages.append(
+        {"role": "system", "content": st.session_state.system_prompt})
     logger.debug("update system_prompt to messages")
 
+
 with st.sidebar:
-    client.base_url = st.text_input("api base", key="openai_api_base", value=os.environ.get("OPENAI_API_BASE", ""))
+    client.base_url = st.text_input(
+        "api base", key="openai_api_base", value=os.environ.get("OPENAI_API_BASE", ""))
     api_key = st.text_input("api key", key="openai_api_key", value=None)
-    client.api_key = api_key if api_key else os.environ.get("OPENAI_API_KEY", "")
-    
+    client.api_key = api_key if api_key else os.environ.get(
+        "OPENAI_API_KEY", "")
+
     model_list = [i.id for i in client.models.list().data]
-    args.model = st.selectbox("Choose your model", model_list,index=1)
+    args.model = st.selectbox("Choose your model", model_list, index=1)
 
     st.text_area(
-        "system prompt", 
+        "system prompt",
         default_system_prompt,
         height=400,
         key="system_prompt",
@@ -64,9 +89,6 @@ with st.sidebar:
     # "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
 
-
-
-
 st.title("💬 Chatbot")
 st.caption("🚀 A streamlit chatbot powered by OpenSource LLM")
 
@@ -74,14 +96,15 @@ if "messages" not in st.session_state:
     st.session_state['messages'] = []
 
     if st.session_state.system_prompt:
-        st.session_state.messages.append({"role": "system", "content": st.session_state.system_prompt})
+        st.session_state.messages.append(
+            {"role": "system", "content": st.session_state.system_prompt})
         logger.debug("update system_prompt to messages")
 
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-if prompt := st.chat_input("Your message"):        
+if prompt := st.chat_input("Your message"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
@@ -95,7 +118,9 @@ if prompt := st.chat_input("Your message"):
             full_response += response.choices[0].delta.content
             message_placeholder.markdown(full_response + "▌")
         message_placeholder.markdown(full_response)
-    st.session_state.messages.append({"role": "assistant", "content": full_response})
+    st.session_state.messages.append(
+        {"role": "assistant", "content": full_response})
+    logger.debug(f"curr messages {st.session_state.messages}")
 
 
 # pip install openai --upgrade
