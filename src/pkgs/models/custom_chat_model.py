b3ddd3fed67733f401e386f663b974a952fe720d
yuanhuachao <yuanhuachao@yuanhuachaodeMacBook-Pro.local>
2024-02-02 14:43:36 +0800

    .

--- src/pkgs/models/custom_chat_model.py
+++ src/pkgs/models/custom_chat_model.py
@@ -171,7 +171,7 @@ class CustomChatModel:
 
     def judge_repeat(self, history, content, model):
         his = [f"\n{i['role']}:{i['content']}" for i in history]
-        his = json.dumps(his, ensure_ascii=False)
+        his = ''.join(his)
         judgge_p = _auxiliary_diagnosis_judgment_repetition_prompt.format(his, content)
         logger.debug(f'问诊重复判断LLM输入：{judgge_p}')
         chat_response = callLLM(
