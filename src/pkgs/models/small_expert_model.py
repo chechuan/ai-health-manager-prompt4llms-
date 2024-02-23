715b91647330a10a7d1d66cb347c0e385e724c92
yuanhuachao <yuanhuachao@yuanhuachaodeMacBook-Pro.local>
2024-02-23 19:23:27 +0800

    .

--- src/pkgs/models/small_expert_model.py
+++ src/pkgs/models/small_expert_model.py
@@ -210,14 +210,15 @@ class expertModel:
         ihm_health_sbp = kwargs['promptParam']['ihm_health_sbp']
         ihm_health_dbp = kwargs['promptParam']['ihm_health_dbp']
         query = kwargs['promptParam'].get('query', '')
-
         def blood_pressure_inquiry(history, query):
             history = [{"role": role_map.get(str(i['role']), "user"), "content": i['content']} for i in history]
             # his_prompt = "\n".join([("Doctor" if not i['role'] == "User" else "User") + f": {i['content']}" for i in history])
             # prompt = blood_pressure_inquiry_prompt.format(blood_pressure_inquiry_prompt) + f'Doctor: '
             messages = [{"role": "system", "content": blood_pressure_inquiry_prompt}] + history
+            logger.debug('血压问诊模型输入： ' + '\n'.join(messages))
             generate_text = callLLM(history=messages, max_tokens=1024, top_p=0.8,
                     temperature=0.0, do_sample=False, model='Qwen-72B-Chat')
+            logger.debug('血压问诊模型输出： ' + generate_text)
             thoughtIdx = generate_text.find("\nThought") + 9
             thought = generate_text[thoughtIdx:].split("\n")[0].strip()
             outIdx = generate_text.find("\nDoctor") + 8
@@ -228,10 +229,12 @@ class expertModel:
         def blood_pressure_pacify(history, query):
             history = [{"role": role_map.get(str(i['role']), "user"), "content": i['content']} for i in history]
             his_prompt = "\n".join([("Doctor" if not i['role'] == "User" else "User") + f": {i['content']}" for i in history])
-            prompt = blood_pressure_pacify_prompt.format(his_prompt) + f'Doctor: '
+            prompt = blood_pressure_pacify_prompt.format(his_prompt)
             messages = [{"role": "user", "content": prompt}]
+            logger.debug('血压安抚模型输入： ' + '\n'.join(messages))
             generate_text = callLLM(history=messages, max_tokens=1024, top_p=0.8,
                     temperature=0.0, do_sample=False, model='Qwen-72B-Chat')
+            logger.debug('血压安抚模型输出： ' + generate_text)
             thoughtIdx = generate_text.find("\nThought") + 9
             thought = generate_text[thoughtIdx:].split("\n")[0].strip()
             outIdx = generate_text.find("\nDoctor") + 8
