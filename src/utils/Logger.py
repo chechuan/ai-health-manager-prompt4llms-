866bb69cce84cc2f4d69d05eb9b4cfac3080aff3
songhaoyanga <songhaoyanga@enn.cn>
2024-03-22 11:16:47 +0800

    update Logger

--- src/utils/Logger.py
+++ src/utils/Logger.py
@@ -1,48 +1,61 @@
 # -*- encoding: utf-8 -*-
-'''
+"""
 @Time    :   2023-03-27 10:23:48
 @Author  :   宋昊阳
 @Contact :   1627635056@qq.com
-'''
-import socket
+"""
+import os
 import sys
-from datetime import datetime
+from functools import partial
 from pathlib import Path
 
 from loguru import logger
 
 logger.remove()
 
+
+def filter_function(appid, record):
+    record["file"].path = (
+        record["file"].path.split(f"{appid}/")[1] if f"{appid}/" in record["file"].path else record["file"].path
+    )
+    return True  # 返回True，表示所有的日志都应被记录
+
+
 class Logging:
-    def __init__(self, appid = "0", console_level='TRACE', file_level = "TRACE"):
+    def __init__(self, appid="0", console_level="TRACE", file_level="DEBUG"):
         """日志记录模块
         :param appid: 应用id
         :param file_level: 保存文件日志logger级别
         """
-        logger.add(sink=sys.stderr, level=console_level, backtrace=True, diagnose=True)
-        try:
-            log_dir = Path("./logs")
-            log_dir.mkdir(exist_ok=True, parents=True)
-        except Exception as error:
-            logger.exception(error)
-            
-        log_path = log_dir.joinpath(f"{appid}.log")
-        log_path.touch(exist_ok=True)
-        
-        format = "[{time:YYYY-MM-DD HH:mm:ss.SSS}] [{level}] [{process}] {module}:{name}:{line} {message}"
+        # global logger
+        LOG_FORMAT = "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{level: <8}</level> | <cyan>{file.path}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>"
+
+        LOG_PATH = Path(__file__).parents[2] / "logs"
+        if not os.path.exists(LOG_PATH):
+            os.mkdir(LOG_PATH)
+
         logger.add(
-            sink=log_path,
-            format=format, 
-            level=file_level, 
-            encoding="utf-8", 
-            rotation="00:00", 
-            retention="10 days", 
-            compression="gz",
-            backtrace=True, 
-            diagnose=True
-            )
-        
+            sink=sys.stderr,
+            level=console_level,
+            format=LOG_FORMAT,
+            backtrace=True,
+            diagnose=True,
+            filter=partial(filter_function, appid),
+        )
+
+        logger.add(
+            sink=LOG_PATH / f"{appid}.log",
+            level=file_level,
+            format=LOG_FORMAT,
+            rotation="50 MB",
+            colorize=False,
+            encoding="utf-8",
+            backtrace=True,
+            diagnose=True,
+        )
+
         self.logger = logger
+
 
 logger = Logging(appid="ai-health-manager-prompt4llms").logger
 
