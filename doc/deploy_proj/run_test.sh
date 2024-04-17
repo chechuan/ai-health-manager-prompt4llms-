4088820e18e3652b6c036d6361db9e4b3e04ba07
songhaoyanga <songhaoyanga@enn.cn>
2024-04-17 17:16:24 +0800

    .

--- doc/deploy_proj/run_test.sh
+++ doc/deploy_proj/run_test.sh
@@ -1,3 +1,5 @@
+export OPENAI_API_KEY=sk-QblVOtsLjhxmmnMUDb9cC741987e4851904e4447BfD38855
+export OPENAI_BASE_URL=http://10.228.67.99:26928
 python src/server.py \
     --env test \
     --port 26921 \
