78cef6f9e63956e8f8f67e28e52dc803fa464d00
ticoAg <1627635056@qq.com>
2024-07-12 18:06:26 +0800

    doc: 增加压测脚本

--- doc/llm_throughput/Qwen2-57B-A14B-Instruct.sh
+++ doc/llm_throughput/Qwen2-57B-A14B-Instruct.sh
@@ -1,9 +1,9 @@
-model=Qwen2-72B-Instruct
-prompt_type=long
+model=Qwen2-57B-A14B-Instruct
+prompt_type=short
 
 python -m src.test.test_llm_throughput \
     --model $model \
     --prompt-type $prompt_type \
-    --base-url http://10.228.67.99:26932/v1 \
+    --base-url http://10.228.67.99:26933/v1 \
     --concurrency-lst "1,5,10,20,50" \
     --alpha-times 10
