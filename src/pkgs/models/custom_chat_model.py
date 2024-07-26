6dff64eb972b7eaa34b7a92c2787089c9788ef01
chengyhb <CHENGYHB@enn.cn>
2024-07-26 14:55:49 +0800

    切割修复

--- src/pkgs/models/custom_chat_model.py
+++ src/pkgs/models/custom_chat_model.py
@@ -113,18 +113,25 @@ class CustomChatAuxiliary(CustomChatModel):
             second_index = s.find(char, first_index + 1)
             return second_index
         try:
+            l1=len(s1)
+            l2=len(s2)
             text =text.replace('：',':')
             thought_index = text.find(s1)
+            if thought_index == -1 :
+                doctor_index = text.find(s2)
+                if doctor_index != -1:
+                    return "None", text[doctor_index + l2 :].strip()
+                else:
+                    return "None", text 
             doctor_index = text.find("\n"+s2)
+            if doctor_index == -1:
+                return "None", text[thought_index + l1 :].strip()
+            thought = text[thought_index + l1 : doctor_index].strip()
             thought_index2=find_second_occurrence(text, "\n"+s1)
-            if thought_index != -1 and doctor_index == -1:
-                return "None", text[thought_index + 8 : doctor_index].strip()
-            if thought_index == -1 and doctor_index != -1:
-                return "None", text[doctor_index + 8 :].strip()
-            if thought_index == -1 and doctor_index == -1:
-                return "None", text
-            thought = text[thought_index + 8 : doctor_index].strip()
-            doctor = text[doctor_index + 8 :thought_index2].strip()
+            if thought_index2 !=-1:
+                doctor = text[doctor_index + l2 :thought_index2].strip()
+            else:
+                doctor = text[doctor_index + l2 :].strip()
             return thought, doctor
         except Exception as err:
             logger.error(text)
@@ -432,12 +439,12 @@ class CustomChatAuxiliary(CustomChatModel):
         thought, output = self.__parse_diff_response__(content,'thought:','output:')
         # 用if_entropy字段来控制不同的场景
         if if_entropy=='0':
-            result = output+'。基于实时监测的物联数据，结合当前节气及天气情况，综合考虑您与家人的生活习惯，生成了三济健康报告:'
+            result = output+'基于实时监测的物联数据，结合当前节气及天气情况，综合考虑您与家人的生活习惯，生成了三济健康报告:'
         elif if_entropy=='1':
             if '；' in output:
                 output=output.split('；',1)[0]
             entropy = pro.get('askEntropy','')
-            result = '叔叔，您的生命熵为'+entropy+'，主要问题是血压不稳定，需要重点控制血压。'+output+'。血压出现一定程度的波动是正常的生理现象，您不必紧张。您可以通过听音乐、阅读、散步等方式来放松心情以保证血压的稳定。'
+            result = '叔叔，您的生命熵为'+entropy+'，主要问题是血压不稳定，需要重点控制血压。'+output+'血压出现一定程度的波动是正常的生理现象，您不必紧张。您可以通过听音乐、阅读、散步等方式来放松心情以保证血压的稳定。'
         else:
             result = '基于实时监测的物联数据，结合当前节气及天气情况，综合考虑您与家人的生活习惯，生成了三济健康报告:'
 
