f27948ff78f3bc65071a98716d54df245d2244e8
ticoAg <1627635056@qq.com>
2024-08-16 17:43:12 +0800

    bugfix: opentelemetry add correct endpoint

--- doc/deploy_proj/run_dev.sh
+++ doc/deploy_proj/run_dev.sh
@@ -4,8 +4,8 @@ export LOG_FILE_LEVEL=TRACE
 
 export OTEL_SERVICE_NAME=ai-health-manager-prompt4llms-dev
 export OTEL_EXPORTER_OTLP_PROTOCOL=http/protobuf
-export OTEL_EXPORTER_OTLP_TRACES_ENDPOINT=http://tracing-analysis-dc-bj-internal.aliyuncs.com/adapt_******/api/otlp/traces
-export OTEL_EXPORTER_OTLP_METRICS_ENDPOINT=http://tracing-analysis-dc-bj-internal.aliyuncs.com/adapt_******/api/otlp/metrics
+export OTEL_EXPORTER_OTLP_TRACES_ENDPOINT=http://tracing-analysis-dc-bj-internal.aliyuncs.com/adapt_da71k5hxwf@a2a96ea73919f75_da71k5hxwf@53df7ad2afe8301/api/otlp/traces
+export OTEL_EXPORTER_OTLP_METRICS_ENDPOINT=http://tracing-analysis-dc-bj-internal.aliyuncs.com/adapt_da71k5hxwf@a2a96ea73919f75_da71k5hxwf@53df7ad2afe8301/api/otlp/metrics
 
 opentelemetry-instrument python src/server.py \
     --port 6500 \
