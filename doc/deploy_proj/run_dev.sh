fbac4356447407c9180209ff4d3186510d7662ff
ticoAg <1627635056@qq.com>
2024-08-16 17:31:11 +0800

    add 链路追踪

--- doc/deploy_proj/run_dev.sh
+++ doc/deploy_proj/run_dev.sh
@@ -1,7 +1,13 @@
 export ENV=dev
 export LOG_CONSOLE_LEVEL=TRACE
 export LOG_FILE_LEVEL=TRACE
-python src/server.py \
+
+export OTEL_SERVICE_NAME=ai-health-manager-prompt4llms-dev
+export OTEL_EXPORTER_OTLP_PROTOCOL=http/protobuf
+export OTEL_EXPORTER_OTLP_TRACES_ENDPOINT=http://tracing-analysis-dc-bj-internal.aliyuncs.com/adapt_******/api/otlp/traces
+export OTEL_EXPORTER_OTLP_METRICS_ENDPOINT=http://tracing-analysis-dc-bj-internal.aliyuncs.com/adapt_******/api/otlp/metrics
+
+opentelemetry-instrument python src/server.py \
     --port 6500 \
     --special_prompt_version
     # --use_proxy
