540a65dd6b1dbec5029549b36d5ff1081d95f174
chechuan <you@example.com>
2024-10-17 16:23:23 +0800

    update

---
+++ copy.py
@@ -0,0 +1,15 @@
+{
+  "default": None,
+  "unit_price": {
+    "amount": 80,
+    "unit": "hour"
+  },
+  "weekday": {
+    "without_meal": None,
+    "with_meal": None
+  },
+  "weekend": {
+    "without_meal": None,
+    "with_meal": None
+  }
+}
