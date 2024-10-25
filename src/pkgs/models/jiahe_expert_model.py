2d15934330068fa51ca5d01f6fa9ab39efcc85aa
chechuan <you@example.com>
2024-10-25 18:10:18 +0800

    feat: 优化了无历史数据时返回 "terminal" 的处理逻辑

--- src/pkgs/models/jiahe_expert_model.py
+++ src/pkgs/models/jiahe_expert_model.py
@@ -96,6 +96,8 @@ class JiaheExpertModel:
                         return {"result": "terminal"}
                     else:
                         return {"result": "order"}
+            else:
+                return {"result": "terminal"}
         else:
             return {"result": "terminal"}
 
