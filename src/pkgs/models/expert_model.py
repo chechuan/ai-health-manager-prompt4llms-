462dccd5b369c47985cb2e9407a6b13b8ebb69c8
CHENGYHB <CHENGYHB@enn.cn>
2024-12-02 14:06:47 +0800

    fix:血糖预警优化

--- src/pkgs/models/expert_model.py
+++ src/pkgs/models/expert_model.py
@@ -1171,11 +1171,11 @@ class expertModel:
                 if glucose < 3:
                     result = "高危低血糖"
                     content='你血糖非常低，请立即补充含糖食物'
-                    agent_content=f"你好，客户目前空腹血糖非常低，请及时与客户取得联系，给予处理建议。"
+                    agent_content=f"""你好，客户目前空腹血糖为{glucose}mmol/L,血糖值非常低，请及时与客户取得联系，给予处理建议。"""
                 elif 3 <= glucose < 3.9:
                     result = "低血糖"
                     content='你血糖较低，请尽快补充含糖食物'
-                    agent_content=f"你好，客户目前空腹血糖偏低，请及时给予处理建议。如果本周发生1-2次低血糖，就属于频繁低血糖，必要时与客户取得联系邀请复诊"
+                    agent_content=f"""你好，客户目前空腹血糖为{glucose}mmol/L,血糖值偏低，请及时给予处理建议。如果本周发生1-2次低血糖，就属于频繁低血糖，必要时与客户取得联系邀请复诊"""
                 elif 3.9 <= glucose <=7:
                     result = "血糖正常"
                     content='血糖正常，请继续保持'
@@ -1183,21 +1183,21 @@ class expertModel:
                 elif 7.0 < glucose <= 13.9:
                     result = "血糖控制高"
                     content='今日空腹血糖值偏高，请遵医嘱，规律生活'
-                    agent_content=f"你好，客户目前空腹血糖偏高，请给予关注。"
+                    agent_content=f"""你好，客户目前空腹血糖为{glucose}mmol/L,血糖值偏高，请给予关注。"""
                 else:
                     result = "血糖控制高危"
                     content = "今日空腹血糖非常高，请严格遵医嘱！"
-                    agent_content="你好，客户目前空腹血糖非常高，请及时关注用户运动量、用药量、饮食量等变化，并进一步与患者沟通，给予改善建议。"
+                    agent_content=f"""你好，客户目前空腹血糖为{glucose}mmol/L,血糖值非常高，请及时关注用户运动量、用药量、饮食量等变化，并进一步与患者沟通，给予改善建议。"""
             elif time == "2" and time != "":
                 t= '餐后2小时血糖'
                 if glucose < 3:
                     result = "高危低血糖"
                     content='你血糖非常低，请立即补充含糖食物'
-                    agent_content=f"你好，客户目前血糖非常低，请及时与客户取得联系，给予处理建议。"
+                    agent_content=f"""你好，客户目前血糖为{glucose}mmol/L,血糖值非常低，请及时与客户取得联系，给予处理建议。"""
                 elif 3 <= glucose < 3.9:
                     result = "低血糖"
                     content='你血糖较低，请尽快补充含糖食物'
-                    agent_content=f"你好，客户目前空腹血糖偏低，请及时与客户取得联系，给予处理建议。"
+                    agent_content=f"""你好，客户目前空腹血糖为{glucose}mmol/L,血糖值偏低，请及时与客户取得联系，给予处理建议。"""
                 elif 3.9 <= glucose <=10:
                     result = "血糖正常"
                     content='当前血糖正常，请继续保持，适量运动'
@@ -1205,21 +1205,21 @@ class expertModel:
                 elif 10 < glucose <= 16.7:
                     result = "血糖控制高"
                     content='餐后2小时血糖值偏高，请遵医嘱，规律生活'
-                    agent_content=f"你好，客户目前餐后2小时血糖偏高，请给予关注。"
+                    agent_content=f"""你好，客户目前餐后2小时血糖为{glucose}mmol/L,血糖值偏高，请给予关注。"""
                 else:
                     result = "血糖控制高危"
                     content = "今日餐后2小时血糖值非常高，请严格遵医嘱"
-                    agent_content=f"你好，客户目前餐后2小时血糖非常高，请及时关注用户运动量、用药量、饮食量等变化，并进一步与患者沟通，给予改善建议。"
+                    agent_content=f"""你好，客户目前餐后2小时血糖为{glucose}mmol/L,血糖值非常高，请及时关注用户运动量、用药量、饮食量等变化，并进一步与患者沟通，给予改善建议。"""
             else:
                 t= '动态血糖'
                 if glucose < 3:
                     result = "高危低血糖"
                     content='你血糖值非常低，请立即补充含糖食物'
-                    agent_content=f"你好，客户目前血糖非常低，请及时与客户取得联系，给予处理建议。"
+                    agent_content=f"""你好，客户目前血糖为{glucose}mmol/L,血糖值非常低，请及时与客户取得联系，给予处理建议。"""
                 elif 3 <= glucose < 3.9:
                     result = "低血糖"
                     content='你血糖较低，请尽快补充含糖食物'
-                    agent_content=f"你好，客户目前血糖偏低，请及时与客户取得联系，给予处理建议。"
+                    agent_content=f"""你好，客户目前血糖为{glucose}mmol/L,血糖值偏低，请及时与客户取得联系，给予处理建议。"""
                 elif 3.9 <= glucose <=10:
                     result = "血糖正常"
                     content='当前血糖正常，请继续保持，适量运动'
@@ -1227,15 +1227,15 @@ class expertModel:
                 elif 10 < glucose <= 13.9:
                     result = "血糖控制高"
                     content='随机血糖值偏高，请遵医嘱，规律生活'
-                    agent_content=f"你好，客户目前随机血糖偏高，请关注该用户近2日动态血糖变化。"
+                    agent_content=f"""你好，客户目前随机血糖为{glucose}mmol/L,血糖值偏高，请关注该用户近2日动态血糖变化。"""
                 elif 13.9 < glucose < 16.7:
                     result = "血糖控制中危"
                     content='随机血糖值较高，请严格遵医嘱，规律生活'
-                    agent_content=f"你好，客户目前随机血糖较高，请关注该用户近2日动态血糖变化，必要时进一步与患者沟通，给予改善建议"
+                    agent_content=f"""你好，客户目前随机血糖为{glucose}mmol/L,血糖值较高，请关注该用户近2日动态血糖变化，必要时进一步与患者沟通，给予改善建议"""
                 else:
                     result = "血糖控制高危"
                     content = "随机血糖值极高，请严格遵医嘱，积极控制血糖！"
-                    agent_content=f"你好，客户目前随机血糖极高，请关注该用户近2日动态血糖变化，必要时进一步与患者沟通，给予改善建议"
+                    agent_content=f"""你好，客户目前随机血糖为{glucose}mmol/L,血糖值极高，请关注该用户近2日动态血糖变化，必要时进一步与患者沟通，给予改善建议"""
             return result,content,agent_content,t
 
         model = "Qwen1.5-32B-Chat"
@@ -1244,7 +1244,7 @@ class expertModel:
         gl_code = pro.get("gl_code", "")
         result,content,agent_content,t=glucose_type(gl_code, gl)
         prompt_template = GLUCOSE_WARNING
-        prompt_vars = {"glucose_data": t+gl+result}
+        prompt_vars = {"glucose_data": t+gl+"mmol/L。"+result}
         sys_prompt = prompt_template.format(**prompt_vars)
 
         history = []
@@ -1255,7 +1255,7 @@ class expertModel:
         )
         dict_={}
         dict_['front']=content
-        dict_['user_warning']="血糖结果"+gl+"mmolL。"+content
+        dict_['user_warning']="血糖结果"+str(float(gl))+"mmol/L。"+content
         dict_['user_content']=response
         dict_['sug_agent']=agent_content
         return dict_
