6979d16ce01f305fe9899ddd84630b2bdf291273
yuanhuachao <yuanhuachao@enn.cn>
2024-12-06 15:36:11 +0800

    替换意图模型

--- chat/qwen_chat.py
+++ chat/qwen_chat.py
@@ -334,7 +334,7 @@ class Chat:
                 # prompt = self.prompt_meta_data['tool']['子意图模版']['description'].format(sub_intent_prompt, h_p) + "\n\n" + query + "\nThought: "
             logger.debug('子意图模型输入：' + prompt)
             generate_text = callLLM(query=prompt, max_tokens=200, top_p=0.8,
-                    temperature=0, do_sample=False, stop=['\nThought'], model='Qwen-14B-Chat')
+                    temperature=0, do_sample=False, stop=['\nThought'], model='Qwen1.5-32B-Chat')
             logger.debug('子意图模型输出：' + generate_text)
             intentIdx = 0
             if 'Intent:' in  generate_text:
