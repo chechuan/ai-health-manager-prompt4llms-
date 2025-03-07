bea07c671804901adc378805a8bce77b30db49ef
chechuan <you@example.com>
2025-03-07 17:29:53 +0800

    feat: mysql连接失败问题兼容

--- src/utils/resources.py
+++ src/utils/resources.py
@@ -41,7 +41,10 @@ class InitAllResource:
         self.__knowledge_connect_check__()
         self.langfuse_client = self.__init_langfuse__()
 
-        self.prompt_meta_data = self.req_prompt_data_from_mysql()
+        try:
+            self.prompt_meta_data = self.req_prompt_data_from_mysql()
+        except Exception as e:
+            self.prompt_meta_data = {}
 
         self.__init_model_supplier__()
         self.client = openai.OpenAI()
@@ -53,13 +56,6 @@ class InitAllResource:
         self.jia_kang_bao_data = self.__load_jia_kang_bao_data__()
         self.jia_kang_bao_data_id_item = {item["id"]: item for item in self.jia_kang_bao_data}
         self.skip_db = os.getenv("SKIP_DB", "false").lower() == "true"
-
-        if self.skip_db:
-            print("[INFO] ⚠️ 数据库加载被跳过，使用默认数据")
-            self.prompt_meta_data = {}  # 这里可以用一个空字典替代数据库内容
-        else:
-            self.prompt_meta_data = self.req_prompt_data_from_mysql()  # 之前的数据库请求
-
 
     def __parse_args__(
         self,
